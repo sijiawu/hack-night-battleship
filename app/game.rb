97cd04cfcd0f7822d668341e309fa9ae826ca13c
Eric Tendian <erictendian@gmail.com>
2019-03-13 20:42:32 -0500

    Starting on sockets

--- app/game.rb
+++ app/game.rb
@@ -1,5 +1,30 @@
-class Game
-  def start()
+require 'socket'
+require_relative './board'
 
+class Game
+  def start_server()
+    server = TCPServer.open(2000)
+    board = Board.new
+    board.randomize_grid()
+    loop {
+       client = server.accept
+       client.puts('a5')
+       client.close
+    }
+  end
+  def start_client()
+    board = Board.new
+    board.randomize_grid()
+
+    hostname = 'localhost'
+    port = 2000
+
+    s = TCPSocket.open(hostname, port)
+
+    while line = s.gets
+      puts line.chop
+      puts board.check_occupied(line.chop)
+    end
+    s.close
   end
 end
